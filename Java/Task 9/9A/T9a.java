380d776248008488aa8838fa739f8d2d5ecdf39b
Venkata Prabhas Guda <prabhasg03@gmail.com>
2023-05-11 11:33:52 +0530

    Create T9a.java

---
+++ Java/Task 9/9A/T9a.java
@@ -0,0 +1,10 @@
+import java.applet.*;
+import java.awt.Graphics;
+/*<applet code="T9a.class" height=200 width=200></applet>*/
+public class T9a extends Applet 
+{
+public void paint(Graphics g)
+{
+g.drawString("hello world",25,50);
+}
+}
