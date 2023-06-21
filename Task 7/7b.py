06d613fc48ed585af0f00874cf046b1e5e2bf02a
Venkata Prabhas <prabhasg03@gmail.com>
2023-06-21 14:00:12 +0530

    dj

--- Task 7/7b.py
+++ Task 7/7b.py
@@ -2,27 +2,28 @@
 The constructor for this class should take two floating-point values. 
 Add methods for adding, subtracting, and multiplying two complex numbers.'''
 class Complex:
-    a=complex(0)
-    def __init__(self,x) -> a:
-        a=complex(x)
-
-class Arithmetic:
-    a,b=complex(0.0),complex(0.0)
     def __init__(self,x,y):
         self.a=x
         self.b=y
     def add(self):
-        return complex(self.a)+complex(self.b)
+        return (self.a)+(self.b)
     def sub(self):
-        return complex(self.a)-complex(self.b)
+        return (self.a)-(self.b)
     def mul(self):
-        return complex(self.a)*complex(self.b)
-
-a=(str(input('Enter Complex number-1').split()))
-b=(str(input('Enter Complex number-2').split()))
-x=Complex(a)
-y=Complex(b)
-t=Arithmetic(x,y)
-print(t.add())
-print(t.sub())
-print(t.mul())
+        return (self.a)*(self.b)
+    
+print("Enter Number in a+bj format only")
+a=(str(input('Enter Complex number-1:')))
+b=(str(input('Enter Complex number-2:')))
+t=Complex(complex(a),complex(b))
+print("Addition:",t.add())
+print("Subtraction:",t.sub())
+print("Multiplication:",t.mul())
+'''Output:
+PS C:/21241A6625> & c:/Users/HP/Desktop/21241A6625/.conda/python.exe "c:/21241A6625/Task 7/7b.py"
+Enter Number in a+bj format only
+Enter Complex number-1:4.4+9.8j
+Enter Complex number-2:5.2+10.4j
+Addition: (9.600000000000001+20.200000000000003j)
+Subtraction: (-0.7999999999999998-0.5999999999999996j)
+Multiplication: (-79.04000000000002+96.72000000000001j)'''
