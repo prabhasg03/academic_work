ae2ab24590899045b31a83fe6d0ae11c85f2af74
Venkata Prabhas Guda <prabhasg03@gmail.com>
2023-07-06 23:49:55 +0530

    Update 10b.py

--- Task 10/10b.py
+++ Task 10/10b.py
@@ -6,27 +6,25 @@ starts.'''
 from tkinter import *
 
 class TrafficLights:
-    def _init_(self):
+    def __init__(self):
         window = Tk()
         window.title("Traffic Light")
         frame = Frame(window)
         frame.pack()
         self.color = IntVar()
-        radio_red = Radiobutton(frame, text="Red", bg="red", variable=self.color, value=1,
-        command=self.on_RadioChange)
+        radio_red = Radiobutton(frame, text="Red", bg="red", variable=self.color, value=1, command=self.on_RadioChange)
         radio_red.grid(row=10, column=1)
-        radio_yellow = Radiobutton(frame, text="Yellow", bg="yellow", variable=self.color,value=2, command=self.on_RadioChange)
-        radio_yellow.grid(row = 10, column = 2) 
-        radio_green = Radiobutton(frame, text="Green", bg="green", variable=self.color, value=3,command=self.on_RadioChange)
-        radio_green.grid(row = 10, column = 3)
+        radio_yellow = Radiobutton(frame, text="Yellow", bg="yellow", variable=self.color, value=2, command=self.on_RadioChange)
+        radio_yellow.grid(row=10, column=2) 
+        radio_green = Radiobutton(frame, text="Green", bg="green", variable=self.color, value=3, command=self.on_RadioChange)
+        radio_green.grid(row=10, column=3)
         self.canvas = Canvas(window, width=450, height=300, bg="white")
         self.canvas.pack()
         self.oval_red = self.canvas.create_oval(10, 10, 110, 110, fill="white")
         self.oval_yellow = self.canvas.create_oval(10, 120, 110, 220, fill="white")
-        self.oval_green = self.canvas.create_oval(10,230, 110, 330, fill="white")
-#self.color.set('R')
-#self.canvas.itemconfig(self.oval_red, fill="red")
+        self.oval_green = self.canvas.create_oval(10, 230, 110, 330, fill="white")
         window.mainloop()
+
     def on_RadioChange(self):
         color = self.color.get()
         if color == 1:
@@ -42,4 +40,4 @@ class TrafficLights:
             self.canvas.itemconfig(self.oval_yellow, fill="white")
             self.canvas.itemconfig(self.oval_green, fill="green")
 
-t=TrafficLights()
+t = TrafficLights()
