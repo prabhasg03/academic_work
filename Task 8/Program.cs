3baebad6a504b8fdb6da5c6aca1ff2588264ef25
prabhasg03 <prabhasg03@gmail.com>
2023-07-12 14:46:23 +0530

    Task10 pending

---
+++ Task 8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+class Program
+{
+    public static void Main(string[] a)
+    {
+        string strcon = @"server=localhost;userid=root@localhost;password=root;database=myshop";
+        using var con = new MySqlConnection(strcon);
+        con.Open();
+        using var cmd = new MySqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = "insert into table1 values (2,'Mohit')";
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Record Inserted");
+    }
+}
