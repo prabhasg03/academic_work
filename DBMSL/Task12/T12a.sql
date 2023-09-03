5942d649d1485658159cc99517f276a68d565aa9
Venkata Prabhas Guda <prabhasg03@gmail.com>
2023-09-03 19:15:51 +0530

    Delete DBMSL directory

--- DBMSL/Task12/T12a.sql
+++
@@ -1,6 +0,0 @@
-create or replace trigger t after update of ename on employees FOR EACH 
-ROW
-begin
-DBMS_OUTPUT.PUT_LINE('Name updated from '||:OLD.ename||' to '||:NEW.ename);
-END;
-/
--- DBMSL/Task13/T13.sql
+++
@@ -1,4 +0,0 @@
-create or replace package p1 is
-procedure t1(id number,choice number);
-END p1;
-/
