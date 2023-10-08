7c4229faf937405dcf0eb77238c82e1606d01c67
Venkata Prabhas Guda <prabhasg03@gmail.com>
2023-10-08 10:44:39 +0530

    Create 3a.R

---
+++ R-Programming/Task 3/3a.R
@@ -0,0 +1,17 @@
+#3a.) Write R program for implementation of built-in functions.
+#Program:
+# Get the list of numbers from the user
+data = (c(1,2,3,4,5))
+print(c("Data:",data))
+print(c("Mean:",mean(data)))
+print(c("Median:",median(data)))
+print(c("Sum:",sum(data)))
+print(c("Sequence:",seq(30,45)))
+#Output:
+#[1] "Data:" "1"     "2"     "3"     "4"     "5"    
+#[1] "Mean:" "3"    
+#[1] "Median:" "3"      
+#[1] "Sum:" "15"  
+#[1] "Sequence:" "30"        "31"        "32"        "33"        "34"       
+#[7] "35"        "36"        "37"        "38"        "39"        "40"       
+#[13] "41"        "42"        "43"        "44"        "45"       
