9693296312345013b3108c89ffd1b55ba60b85ac
Venkata Prabhas Guda <prabhasg03@gmail.com>
2024-03-17 21:49:49 +0530

    Update 6b.R

--- R-Programming/Task 6/6b.R
+++ R-Programming/Task 6/6b.R
@@ -1,4 +1,13 @@
+#TASK 6
+#(b) Write a R program for multiplication of two matrices.
 a = matrix(c(1,3,5,7), ncol=2, nrow=2)
 b = matrix(c(2,4,6,8), ncol=2, nrow=2)
 a*b
 a %*% b
+#Output:
+#     [,1] [,2]
+#[1,]    2   30
+#[2,]   12   56
+#     [,1] [,2]
+#[1,]   22   46
+#[2,]   34   74
